5034a0091702d3bd4fb0b4db5baeca78ca81e7a6
arappl <anja.rappl@fau.de>
2018-12-11 17:22:08 +0100

    estimation function converge

---
+++ f_estfun.R
@@ -0,0 +1,71 @@
+estfun <- function(x, model, n, n_i, alpha, beta, betas, betals, betatimeind, lambda, betat = 0, noninf = 0, noninfs = 0, 
+                    noninfls = 0) {
+  repeat{
+    gc()
+    # +++++ Data Simulation +++++ # 
+    # simulate data with specific seed, if fits fail, change the seed (i <-  i + 1) and try again
+    i <- i + 1
+    # print(c(x, i))
+    seed <- 8469 + x*1000 + i
+    set.seed(seed)
+    dat <- simJM(n = n, n_i = n_i, alpha = alpha, beta = beta, betas = betas, betals = betals, betatimeind = betatimeind, lambda = lambda, 
+                 betat = betat, noninf = noninf, noninfs = noninfs, noninfls = noninfls)
+    
+    df <- with(dat, data.frame(id, y, X, Xls))
+    df.id <- with(dat, data.frame(unique(id), Xs, T_surv, delta))
+    
+    # little workaround to be able to run JM flexibly across various models: jointModel() resorts to the formula call of lme(). If 
+    # this formula is passed down through a function to lme() (like in mjoint()), the lme output stores a string of this function. When 
+    # jointModel() tries to convert this string into a formula again it will run into an error. Therefore we'll store a code snippet with 
+    # the model specific formula in a seperate file and source it back in. Thus jointModel() can access a correct formula and we can only 
+    # one function instead of six (or more respectively) versions of it :).
+    if (x == 1) { 
+      file_ini <- file(paste0(path, "/", sub_dir, "/", "Model_", model, ".txt"))
+      
+      writeLines(c(
+        "lmeIni <- function(df = df){",
+        paste0("fitLME <- tryCatch({ lme(", paste0("y~", paste(names(df)[3 : ncol(df)], collapse = "+")),","),
+        "                             data = df, random = ~ time | id, na.action = na.exclude, control =lmeControl(opt='optim')) }, ",
+        paste0("           error=function(e){cat(","'ERROR :'", ", conditionMessage(e),"," '/n'",")}) "),
+        "}"
+      )
+      , file_ini)
+      
+      close(file_ini)
+    }
+    
+    # +++++ JM fit +++++ # 
+    source(paste0(path, "/", sub_dir, "/", "Model_", model, ".txt"))
+    fitLME <- lmeIni(df = df)
+    fitSURV <- coxph(Surv(time = T_surv, event=delta)~ . -id, data = df.id, x = TRUE, na.action = na.exclude)
+    fitJM <-  tryCatch({ jointModel(fitLME, fitSURV, timeVar = "time", method="piecewise-PH-aGH")
+    }, error=function(e){cat("ERROR :",conditionMessage(e), "/n")})
+    
+    # +++++ joineRML fit +++++ # 
+    # if fitJM returnd no error, fit joineRML
+    if (!is.error(fitJM) && !is.null(fitJM)) {
+      fitJRML <- tryCatch({mjoint(formLongFixed = list(as.formula(paste0("y~",paste(names(df)[3:(ncol(df)-3)], collapse="+")))),
+                                  formLongRandom = list(~ time | id),
+                                  formSurv = Surv(time = T_surv, event = delta) ~ . -id,
+                                  data = list(df),
+                                  survData = df.id, 
+                                  timeVar = "time")},
+                          error=function(e){cat("ERROR :",conditionMessage(e), "/n")})
+      # if fitJRML is also no error, break the repeat loop, else increase i and try again
+      if (!is.error(fitJRML) && !is.null(fitJRML)) { break }
+    }
+  }
+  # +++++ JMboost 'fit' +++++ # 
+  if (betatimeind == 0) {
+    time.effect <- FALSE
+  } else {
+    time.effect <- TRUE
+  }
+  
+  fitJMb <- tryCatch({JMboostc(y = dat$y, Xl = dat$X, Xs = dat$Xs, Xls = dat$Xls, time.effect = time.effect,
+                               delta = dat$delta, T_long = dat$T_long, T_surv = dat$T_surv, id = dat$id, 
+                               mstop_l = 100000, mstop_ls = 100000, mstop_s = 100000, verbose = FALSE)},
+                     error=function(e){cat("ERROR :",conditionMessage(e), "/n")})
+  
+  return(list(fitJM, fitJRML, fitJMb, seed))
+}
